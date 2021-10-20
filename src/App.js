42e75cb4d299b8f25bd284e329ca2d5d24a4569c
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-10-20 17:20:37 -0400

    step 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import fetchGraphQL from './fetchGraphQL'
+import { useState, useEffect }from 'react'
 
 function App() {
+  const [name, setName] = useState(null)
+
+  useEffect(() => {
+    let isMounted = true;
+
+    fetchGraphQL(`#graphql
+      query RepositoryNameQuery {
+        repository(owner: "mrv1k" name: "garage") {
+          name
+        }
+      }
+    `).then((response) => {
+      // Avoid updating state if the component unmounted before the fetch completes
+      if (!isMounted) {
+        return;
+      }
+      const data = response.data;
+      setName(data.repository.name);
+    }).catch((error) => {
+      console.error(error);
+    });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {name !== null ? `Repository: ${name}` : "Loading"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
