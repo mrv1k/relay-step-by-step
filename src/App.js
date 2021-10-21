7f9e6bf2b0ec9c57d80e95a7c6af2c9a4ee3a759
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-10-21 10:22:56 -0400

    step 5

--- src/App.js
+++ src/App.js
@@ -1,44 +1,64 @@
 import './App.css';
-import fetchGraphQL from './fetchGraphQL'
-import { useState, useEffect }from 'react'
+// import fetchGraphQL from './fetchGraphQL';
+import graphql from 'babel-plugin-relay/macro';
+import {
+  RelayEnvironmentProvider,
+  loadQuery,
+  usePreloadedQuery,
+} from 'react-relay/hooks';
+import RelayEnvironment from './RelayEnvironment';
+import { Suspense } from 'react';
 
-function App() {
-  const [name, setName] = useState(null)
+// Define a query
+const RepositoryNameQuery = graphql`
+  query AppRepositoryNameQuery {
+    repository(owner: "mrv1k", name: "garage") {
+      name
+      description
+    }
+  }
+`;
 
-  useEffect(() => {
-    let isMounted = true;
+// Immediately load the query as our app starts. For a real app, we'd move this
+// into our routing configuration, preloading data as we transition to new routes.
+const preloadedQuery = loadQuery(RelayEnvironment, RepositoryNameQuery, {
+  /* query variables */
+});
 
-    fetchGraphQL(`#graphql
-      query RepositoryNameQuery {
-        repository(owner: "mrv1k" name: "garage") {
-          name
-        }
-      }
-    `).then((response) => {
-      // Avoid updating state if the component unmounted before the fetch completes
-      if (!isMounted) {
-        return;
-      }
-      const data = response.data;
-      setName(data.repository.name);
-    }).catch((error) => {
-      console.error(error);
-    });
-
-    return () => {
-      isMounted = false;
-    };
-  }, [])
+// Inner component that reads the preloaded query results via `usePreloadedQuery()`.
+// This works as follows:
+// - If the query has completed, it returns the results of the query.
+// - If the query is still pending, it "suspends" (indicates to React that the
+//   component isn't ready to render yet). This will show the nearest <Suspense>
+//   fallback.
+// - If the query failed, it throws the failure error. For simplicity we aren't
+//   handling the failure case here.
+function App(props) {
+  const data = usePreloadedQuery(RepositoryNameQuery, props.preloadedQuery);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {name !== null ? `Repository: ${name}` : "Loading"}
-        </p>
+        <p>{data.repository.name}</p>
+        <p>{data.repository.description}</p>
       </header>
     </div>
   );
 }
 
-export default App;
+// The above component needs to know how to access the Relay environment, and we
+// need to specify a fallback in case it suspends:
+// - <RelayEnvironmentProvider> tells child components how to talk to the current
+//   Relay Environment instance
+// - <Suspense> specifies a fallback in case a child suspends.
+function AppRoot(props) {
+  return (
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <Suspense fallback={'Loading...'}>
+        <App preloadedQuery={preloadedQuery} />
+      </Suspense>
+    </RelayEnvironmentProvider>
+  );
+}
+
+export default AppRoot;
---
+++ src/__generated__/AppRepositoryNameQuery.graphql.js
@@ -0,0 +1,127 @@
+/**
+ * @flow
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ConcreteRequest } from 'relay-runtime';
+export type AppRepositoryNameQueryVariables = {||};
+export type AppRepositoryNameQueryResponse = {|
+  +repository: ?{|
+    +name: string,
+    +description: ?string,
+  |}
+|};
+export type AppRepositoryNameQuery = {|
+  variables: AppRepositoryNameQueryVariables,
+  response: AppRepositoryNameQueryResponse,
+|};
+*/
+
+
+/*
+query AppRepositoryNameQuery {
+  repository(owner: "mrv1k", name: "garage") {
+    name
+    description
+    id
+  }
+}
+*/
+
+const node/*: ConcreteRequest*/ = (function(){
+var v0 = [
+  {
+    "kind": "Literal",
+    "name": "name",
+    "value": "garage"
+  },
+  {
+    "kind": "Literal",
+    "name": "owner",
+    "value": "mrv1k"
+  }
+],
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+},
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "description",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "AppRepositoryNameQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "Repository",
+        "kind": "LinkedField",
+        "name": "repository",
+        "plural": false,
+        "selections": [
+          (v1/*: any*/),
+          (v2/*: any*/)
+        ],
+        "storageKey": "repository(name:\"garage\",owner:\"mrv1k\")"
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "AppRepositoryNameQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "Repository",
+        "kind": "LinkedField",
+        "name": "repository",
+        "plural": false,
+        "selections": [
+          (v1/*: any*/),
+          (v2/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": "repository(name:\"garage\",owner:\"mrv1k\")"
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "575a318ea1a193be9d9dedc12f718f67",
+    "id": null,
+    "metadata": {},
+    "name": "AppRepositoryNameQuery",
+    "operationKind": "query",
+    "text": "query AppRepositoryNameQuery {\n  repository(owner: \"mrv1k\", name: \"garage\") {\n    name\n    description\n    id\n  }\n}\n"
+  }
+};
+})();
+// prettier-ignore
+(node/*: any*/).hash = '2f7f9473d439d30c6bfbec0282442ff7';
+
+module.exports = node;
